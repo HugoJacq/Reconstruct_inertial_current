dbd81a30ee55782d18de2657fa9f538f138cadf8
HugoJacq <hjacquet974@gmail.com>
2025-02-14 08:56:45 +0100

    up

--- post_process_gotm.py
+++ post_process_gotm.py
@@ -13,7 +13,7 @@ from constants import *
 # ===================================
 # INPUT
 workdir = 'gotm_workdir/'
-exp = 'Croco_LON-50.0_LAT35.0/'
+
 point_loc = [-53.,29.5] # °E, °N
 file_name = 'output_file.nc'
 forcing_file = '/home/jacqhugo/Datlas_2025/DATA_Crocco/croco_1h_inst_surf_2006-02-01-2006-02-28.nc'
@@ -28,30 +28,42 @@ dir_save = "./png_gotm/"
 dpi=200
 # ===================================
 
+exp = 'Croco_LON' + str(point_loc[0])+'_'+'LAT' + str(point_loc[1])+'/'
+
 path_save = dir_save + exp
 if not pathlib.Path(path_save).is_dir():
     os.system('mkdir '+path_save)
 
 dsout = xr.open_dataset(workdir+exp+file_name).isel(lon=0,lat=0)
-dstruth = open_croco_sfx_file_at_point_loc(forcing_file, point_loc)
+# this can be long because of Cgrid that requires interpolation 
+#  |
+#  v
+dstruth = open_croco_sfx_file_at_point_loc(forcing_file, point_loc) 
 
 time_gotm = np.arange(0,len(dsout.time))*dt_gotm
 time_croco = np.arange(0,len(dstruth.time))*dt_croco
 
 Ugotm = dsout.u
 Ucroco = dstruth.U
-
+Taux = dstruth.oceTAUX
+Tauy = dstruth.oceTAUY
 #print('Ugotm',Ugotm)
 #print('Ucroco',Ucroco)
 
 # looking at surface current gotm vs Croco
 if PLOT_CURRENT:
-    fig, ax = plt.subplots(1,1,figsize = (10,5),constrained_layout=True,dpi=dpi)
-    ax.plot( time_gotm/oneday, Ugotm[:,-1], c='b',label='gotm')
-    ax.plot( time_croco/oneday, Ucroco, c='k',label='croco')
-    ax.set_xlabel('time (days)')
-    ax.set_ylabel('surface current (m/s)')
-    ax.legend()
+    fig, ax = plt.subplots(2,1,figsize = (10,7),constrained_layout=True,dpi=dpi)
+    ax[0].plot( time_gotm/oneday, Ugotm[:,-1], c='b',label='gotm')
+    ax[0].plot( time_croco/oneday, Ucroco, c='k',label='croco')
+    ax[0].set_ylabel('surface current (m/s)')
+    ax[0].legend()
+    
+    ax[1].plot(time_croco/oneday, Taux, c='limegreen',label=r'$\tau_x$')
+    ax[1].plot(time_croco/oneday, Tauy, c='darkorange',label=r'$\tau_y$')
+    ax[1].set_ylabel('wind stress (m2/s2)')
+    ax[1].set_xlabel('time (days)')
+    ax[1].legend()
+    
     fig.savefig(path_save+'gotm_vs_Croco_Usfx.png')
     
 
